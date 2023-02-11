e237a95ca54dbfbb53530292cce8780398d152b6
Caio Lobo <caioeduardolobo@gmail.com>
2023-02-11 15:16:01 -0300

    create JwtAuthenticationFilter

---
+++ src/main/java/br/com/caiolobo/blogapplication/config/JwtService.java
@@ -0,0 +1,10 @@
+package br.com.caiolobo.blogapplication.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String jwtToken) {
+        return null;
+    }
+}
