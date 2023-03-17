2e58b0ba924ed998cc7ed1ba23bf425b9ffe62aa
Caio Lobo <caioeduardolobo@gmail.com>
2023-03-17 14:14:49 -0300

    Autowired AccountRepository on ApplicationConfig

--- src/main/java/br/com/caiolobo/blogapplication/config/ApplicationConfig.java
+++ src/main/java/br/com/caiolobo/blogapplication/config/ApplicationConfig.java
@@ -2,13 +2,13 @@ package br.com.caiolobo.blogapplication.config;
 
 import br.com.caiolobo.blogapplication.repositories.AccountRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,8 +17,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
-
-    private final AccountRepository accountRepository;
+    @Autowired
+    private AccountRepository accountRepository;
 
     @Bean
     public UserDetailsService userDetailsService() throws UsernameNotFoundException{    //loadUserByUsername
--- src/main/java/br/com/caiolobo/blogapplication/repositories/AccountRepository.java
+++ src/main/java/br/com/caiolobo/blogapplication/repositories/AccountRepository.java
@@ -2,7 +2,6 @@ package br.com.caiolobo.blogapplication.repositories;
 
 import br.com.caiolobo.blogapplication.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
