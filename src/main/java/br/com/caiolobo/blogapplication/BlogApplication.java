31c8d8fd6fd182ba5605bdb293fa8088f98ef252
Caio Lobo <caioeduardolobo@gmail.com>
2023-03-17 14:10:48 -0300

    add exclude DataSourceAutoConfiguration

--- src/main/java/br/com/caiolobo/blogapplication/BlogApplication.java
+++ src/main/java/br/com/caiolobo/blogapplication/BlogApplication.java
@@ -2,8 +2,9 @@ package br.com.caiolobo.blogapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
