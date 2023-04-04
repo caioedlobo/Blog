5ed412a91be0bd6c8ba74725c0897a16c5b2901b
Caio Lobo <caioeduardolobo@gmail.com>
2023-04-04 13:51:53 -0300

    add cors to securityfilterchain

--- src/main/java/br/com/caiolobo/blogapplication/config/SecurityConfiguration.java
+++ src/main/java/br/com/caiolobo/blogapplication/config/SecurityConfiguration.java
@@ -51,7 +51,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .csrf().disable()
+                .cors().and().csrf().disable()
                 .authorizeHttpRequests()
                 .requestMatchers(AUTH_WHITELIST)
                 .permitAll()
