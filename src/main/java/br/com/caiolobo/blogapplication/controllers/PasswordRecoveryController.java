2712ee61cdeeb5eac44c56d1c1cd8a9f53ad8e1d
Caio Lobo <caioeduardolobo@gmail.com>
2023-04-12 15:44:13 -0300

    create sibmitChangePassword on PasswordRecoveryController

--- src/main/java/br/com/caiolobo/blogapplication/controllers/PasswordRecoveryController.java
+++ src/main/java/br/com/caiolobo/blogapplication/controllers/PasswordRecoveryController.java
@@ -1,8 +1,10 @@
 package br.com.caiolobo.blogapplication.controllers;
 
+import br.com.caiolobo.blogapplication.auth.AuthenticationRequest;
 import br.com.caiolobo.blogapplication.models.RecoveryPasswordRequest;
 import br.com.caiolobo.blogapplication.services.PasswordRecoveryService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.token.Token;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +18,14 @@ public class PasswordRecoveryController {
     }
 
     @PostMapping
-    public ResponseEntity<String> generateToken(@RequestBody RecoveryPasswordRequest request) throws Exception {
+    public ResponseEntity generateToken(@RequestBody RecoveryPasswordRequest request) throws Exception {
         passwordRecoveryService.generateToken(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{token}")
+    public ResponseEntity submitChangePassword(@RequestParam("token") Token token, AuthenticationRequest request){
+        passwordRecoveryService.changePassword(request.getPassword(), token);
         return ResponseEntity.ok().build();
     }
 }
