423d56e8ab769287db0d6598822cf33ab5178b11
Caio <caioeduardolobo@gmail.com>
2023-05-05 16:23:50 -0300

    change response status on register account

--- src/main/java/br/com/caiolobo/blogapplication/controllers/AuthenticationController.java
+++ src/main/java/br/com/caiolobo/blogapplication/controllers/AuthenticationController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -34,7 +35,7 @@ public class AuthenticationController {
     @ApiResponse(responseCode = "201")
     @PostMapping(value = "/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody @Valid RegisterRequest request) {
-        return ResponseEntity.ok(authenticationService.register(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(authenticationService.register(request));
     }
 
     @Operation(summary = "Authenticate user")
